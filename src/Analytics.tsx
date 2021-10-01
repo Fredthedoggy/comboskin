920ee62371c74e4cd46578036c576d81dfff8f41
Fredthedoggy <45927799+fredthedoggy@users.noreply.github.com>
2021-09-30 21:57:36 -0400

    Analytics.

--- src/Analytics.tsx
+++ src/Analytics.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import React from 'react';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 export function Analytics() {
     const location = useLocation();
