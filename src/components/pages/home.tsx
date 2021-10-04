90fd3a36ace7a363bfde53a6f0f080950892c6a8
Fredthedoggy <45927799+fredthedoggy@users.noreply.github.com>
2021-10-04 14:17:21 -0400

    Other Effects Tab

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -4,6 +4,10 @@ import React, { Suspense } from 'react';
 import { Async } from 'react-async';
 import SkinMerger from '../../SkinMerger';
 import Gridtile from './skins/gridtile';
+import { css } from 'styled-components';
+import { faDice } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 
 export default function Home({ data }: { data: ComboAPI[] }) {
     return (
@@ -23,13 +27,36 @@ export default function Home({ data }: { data: ComboAPI[] }) {
                                 }
                             >
                                 {({ data, isLoading }: { data: string; isLoading: boolean }) => {
-                                    return <Gridtile skin={isLoading? undefined : data} display={image.name} short={image.short} to={'view'} />
+                                    return (
+                                        <Gridtile
+                                            skin={isLoading ? undefined : data}
+                                            display={image.name}
+                                            short={image.short}
+                                            to={'view'}
+                                        />
+                                    );
                                 }}
                             </Async>
                         </Suspense>
                     </div>
                 );
             })}
+            <Link to={'/other'}>
+                <div
+                    css={css`
+                        height: 444px;
+                        width: 258px;
+                        ${tw`rounded-lg shadow-md border-gray-200 border-4 mx-auto flex`}
+                    `}
+                >
+                    <span css={tw`m-auto text-gray-500 flex flex-col text-lg`}>
+                        Other Effects
+                        <span css={tw`mx-auto mt-2`}>
+                            <FontAwesomeIcon icon={faDice} size={'2x'} />
+                        </span>
+                    </span>
+                </div>
+            </Link>
         </div>
     );
 }
