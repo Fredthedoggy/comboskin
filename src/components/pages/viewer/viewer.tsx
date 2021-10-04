041c28019cd6b76b701a8615f5f807611c636cb4
Fredthedoggy <45927799+fredthedoggy@users.noreply.github.com>
2021-10-04 14:27:36 -0400

    Add Hash based merge saving

--- src/components/pages/viewer/viewer.tsx
+++ src/components/pages/viewer/viewer.tsx
@@ -7,9 +7,10 @@ const steve =
     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAAXNSR0IArs4c6QAAAIRJREFUeF7t1QERADAMArHi33SFfOagHBm7+Fv8/hOABsQTQCBeAJ8gAgjEE0AgXgArgAAC8QQQiBfACiCAQDwBBOIFsAIIIBBPAIF4AawAAgjEE0AgXgArgAAC8QQQiBfACiCAQDwBBOIFsAIIIBBPAIF4AawAAgjEE0AgXgArgECdwANo2ABBrP9ggQAAAABJRU5ErkJggg==';
 export default function Viewer({ skinDetails }: { combo: string; skinDetails: ComboAPI }) {
     const [skinData, setSkinData] = useState<string>(steve);
+    const skins = window.location.hash && window.location.hash.length > 1 ? window.location.hash.slice(1).split('&'): []
 
     const [inputs, setInputs] = useState(
-        Array.from(Array(skinDetails.amount).keys()).map((n) => localStorage.getItem('skin' + n) ?? ''),
+        Array.from(Array(skinDetails.amount).keys()).map((n) => skins[n] ?? localStorage.getItem('skin' + n) ?? ''),
     );
 
     useEffect(() => {
